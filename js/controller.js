ec46d6ad6c19bc3b1d79b5162192ecface80b759
welberthenrique <welberthenrique@gmail.com>
2021-09-04 12:43:55 -0300

    Adicionado arquivo index e modificações no arquivo js

---
+++ js/controller.js
@@ -0,0 +1,16 @@
+/*
+ - Codigo do curso de lógica: 04/09/2021
+ - Programador: Welbert Araujo
+*/
+
+
+var nome = 'Renato Oliveira';
+var peso = 76.00;
+var altura = 1.78;
+var imc = peso / (altura**2); //imc
+
+
+
+console.log(nome);
+console.log(`Peso: ${peso} - Altura: ${altura}`);
+console.log(`Calculo do IMC: ${imc.toFixed(2)}`);
